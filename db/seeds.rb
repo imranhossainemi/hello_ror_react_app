c83d62d2e79338f82de2106787a8c899c480cd84
Imran Hossain <blur-imran@hotmail.com>
2023-07-18 11:01:33 +0600

    Update seeds.rb
    manual injection of greetings run this command: rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = ['Hello', 'Hi', 'Greetings', 'Salutations', 'Welcome']
+
+greetings.each do |greeting|
+  Message.create(content: greeting)
+end
+
